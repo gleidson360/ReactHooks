eead9e45dd2f778b67278265559caabb49cd1bbf
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2023-10-21 14:43:43 -0300

    Estudando useState

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState("Gleidson")
+  const [number, setNumber] = useState(1)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Meu nome é {name}.</Text>
+
+      <TextInput
+        onChangeText={setName}
+        value={name}
+        style= {styles.input}
+      />
+
+      <Text>O número é {number}.</Text>
     </View>
   );
 }
@@ -17,4 +28,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10, 
+  }
 });
